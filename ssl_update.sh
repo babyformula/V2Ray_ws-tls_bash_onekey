d91f568d8d9b0fbdd172c825e37a650ae94162f6
babyformula <spongebobobo@gmail.com>
2021-06-07 17:16:40 +0800

    Update ssl_update.sh
    add 2nd optional domain to cron update the certs.

--- ssl_update.sh
+++ ssl_update.sh
@@ -4,10 +4,11 @@ export PATH
 
 v2ray_qr_config_file="/usr/local/vmess_qr.json"
 domain=$(grep '\"add\"' $v2ray_qr_config_file | awk -F '"' '{print $4}')
+domain_bak=$(grep '\"add_bak\"' $v2ray_qr_config_file | awk -F '"' '{print $4}')
 
 systemctl stop nginx &> /dev/null
 sleep 1
 "/root/.acme.sh"/acme.sh --cron --home "/root/.acme.sh" &> /dev/null
-"/root/.acme.sh"/acme.sh --installcert -d ${domain} --fullchainpath /data/v2ray.crt --keypath /data/v2ray.key --ecc
+"/root/.acme.sh"/acme.sh --installcert -d ${domain} -d ${domain_bak} --fullchainpath /data/v2ray.crt --keypath /data/v2ray.key --ecc --force
 sleep 1
 systemctl start nginx &> /dev/null
